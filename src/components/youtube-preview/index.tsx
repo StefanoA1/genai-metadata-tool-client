8302cbcc185ec16a2db142dad3c49eec8ae51ee6
Anto Ballu <113359769+antonia2929@users.noreply.github.com>
2023-05-10 18:15:35 +0200

    get shortenUrl (#9)

--- src/components/youtube-preview/index.tsx
+++ src/components/youtube-preview/index.tsx
@@ -8,14 +8,15 @@ interface YouTubePreviewProps {
 }
 const extractVideoId = (url: string): string => {
   const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\\?]*).*/;
+  // const regExp = /(?:\?v=|\/)([a-zA-Z0-9_-]{11})/;
   const match = url.match(regExp);
-
   return match && match[2].length === 11 ? match[2] : null;
 };
 
 export const YoutubePreview = (props: YouTubePreviewProps): JSX.Element => {
   const {videoUrl} = props;
-  const videoId = extractVideoId(videoUrl);
+  const shortenUrl = videoUrl.split('%')[0];
+  const videoId = extractVideoId(shortenUrl);
   return videoUrl ? (
     <div className={style.youtubePreviewContainer}>
       <AppTitle title={'YouTube Video Preview'} />
