25692826702f2cc00c3510ded16ed2688a2dae43
stefano <stefanoacosta.92@gmail.com>
2023-05-10 14:04:49 +0200

    Follow ups

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import style from './app-style.css';
 import Button from './components/button';
 import type {ButtonProps} from './components/button/types';
 import {Glossary} from './components/glossary';
-import {InputText} from './components/input-text';
+import {InputText, InputTextProps} from './components/input-text';
 
 const INPUT_FIELD_THEME = 'coorpmanager';
 
@@ -18,8 +18,7 @@ export const App: FunctionComponent = () => {
   const onUrlInput = useCallback((value: string) => setVideoUrl(value), []);
 
   // ----- url -----
-  const urlInputProps = {
-    theme: INPUT_FIELD_THEME,
+  const urlInputProps: InputTextProps = {
     onChange: onUrlInput,
     title: 'Video url',
     placeholder: 'some.video.url/example123',
@@ -41,8 +40,7 @@ export const App: FunctionComponent = () => {
   // ----- Basic section -----
 
   // upper section
-  const titleProps = {
-    theme: INPUT_FIELD_THEME,
+  const titleProps: InputTextProps = {
     disabled: true,
     // onChange: onUrlInput,
     title: 'Title',
@@ -51,8 +49,7 @@ export const App: FunctionComponent = () => {
   };
 
   // lower section
-  const languageProps = {
-    theme: INPUT_FIELD_THEME,
+  const languageProps: InputTextProps = {
     disabled: true,
     // onChange: onUrlInput,
     title: 'Language',
@@ -89,14 +86,32 @@ export const App: FunctionComponent = () => {
       "Vastness is bearable only through love Flatland billions upon billions Jean-François Champollion paroxysm of global death something incredible is waiting to be known? The sky calls to us dispassionate extraterrestrial observer realm of the galaxies made in the interiors of collapsing stars tingling of the spine tingling of the spine. Dream of the mind's eye two ghostly white figures in coveralls and helmets are softly dancing dream of the mind's eye are creatures of the cosmos two ghostly white figures in coveralls and helmets are softly dancing not a sunrise but a galaxyrise."
   };
 
+  const followUpsProps = {
+    theme: INPUT_FIELD_THEME,
+    disabled: true,
+    // onChange: onUrlInput,
+    title: 'Follow ups',
+    placeholder: 'Useful resources to follow on learning',
+    value:
+      '1. Aurélien Géron - "Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow: Concepts, Tools, and Techniques to Build Intelligent Systems" - O\'Reilly Media, 2019 - A practical guide for machine learning beginners and practitioners. https://www.goodreads.com/book/show/38886397-hands-on-machine-learning-with-scikit-learn-keras-and-tensorflow\n\n2. Andrew Ng - "Machine Learning Yearning" - Goodfellow Publishers, 2018 - A book that gives practical advice on how to implement a successful machine learning program'
+  };
+
+  // const keyPhrasesProps: InputTextProps = {
+  //   disabled: true,
+  //   // onChange: onUrlInput,
+  //   title: 'Key Phrases',
+  //   placeholder: 'Content Key Phrases',
+  //   value: 'This is a really important, super key phrase'
+  // };
+
   return (
     <div className={style.appContainer}>
       <AppTitle title={'Turbo Learning'} isBigTitle />
       <div className={style.urlInputContainer}>
-        <div className={style.inputWrapper}>
+        <div className={style.inputContainer}>
           <InputText {...urlInputProps} />
         </div>
-        <div className={style.generateButtonWrapper}>
+        <div className={style.generateButtonContainer}>
           <Button {...generateButtonProps} />
         </div>
       </div>
@@ -106,7 +121,7 @@ export const App: FunctionComponent = () => {
             <InputText {...titleProps} />
           </div>
           <div className={style.basicLowerSection}>
-            <div className={style.imageWrapper}>
+            <div className={style.imageContainer}>
               <img
                 src="https://placehold.co/256x256/png"
                 alt="your image description"
@@ -122,6 +137,14 @@ export const App: FunctionComponent = () => {
           <div className={style.detailedSummarySection}>
             <InputTextArea {...detailedSummaryProps} />
           </div>
+          <div className={style.followUpsContainer}>
+            <InputTextArea {...followUpsProps} />
+          </div>
+
+          {/* <div className={style.keyPhrasesContainer}>
+            <InputTextArea {...followUpsProps} />
+          </div> */}
+
           <AppTitle title={'Glossary'} />
           <Glossary
             definitions={[
@@ -129,13 +152,10 @@ export const App: FunctionComponent = () => {
               ['Another Test', 'This is another test']
             ]}
           />
-          {/* <div className={style.preview}>
-
-          </div> */}
           <div />
         </div>
       ) : null}
-      <div className={style.exportButtonWrapper}>
+      <div className={style.exportButtonContainer}>
         <Button {...exportButtonProps} />
       </div>
     </div>
